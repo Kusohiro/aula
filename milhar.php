50f466496fac5b2c97e9ce40ca9a38e83da2da3e
クソヒーロー <85805893+Kusohiro@users.noreply.github.com>
2021-08-13 12:35:37 -0300

    Update milhar.php

--- milhar.php
+++ milhar.php
@@ -5,18 +5,27 @@
 <title>jogo do bicho</title>
 </head>
 <?php 
-
+	$bicho = "";
+	$animal = array("avestruz","aguia","burro","borboleta","cachorro","cabra","carneiro","camelo","cobra","coelho","cavalo","elefante","galo","gato","jacare","leão","macaco","porco","pavão","peru","touro","tigre","urso","veado","vaca");
 	$milhar = isset($_GET['milhar']) ? $_GET['milhar'] : "";
 	$aposta = isset($_GET['aposta']) ? $_GET['aposta'] : ""
 	?>
 <body>
-	 <?php include "menu.php" ?>
+	 <?php include "menu.php";
+	include "funções.php";
+	?>
 	<form method="get">
 	valor de aposta (r$):<input type="text" name="aposta" id="aposta" <?php if(isset($_GET['aposta'])) echo "value='", $aposta, "'"?></input> <br>
 	milhar:<input type="text" name="milhar" id="milhar" <?php if(isset($_GET['milhar'])) echo "value='", $milhar, "'"?>
 				 </input> <br>
 	<input type="submit" value="jogar">
 	</form>
-	<?php echo $milhar, "<br>",$aposta; ?>
+	<?php echo $milhar, "<br>",$aposta, "<br>"; 
+	$vencer = 0;
+	for($x = 0; $x < 5; $x++){
+	sorteio($milhar, $bicho);
+		//if ($vencer==1){ echo "VENCEDOR! <br> <br>";}
+	}
+	?>
 </body>
 </html>
